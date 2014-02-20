885578c413808b3d60d1c0f89b29be72172f9691
Davert <davert.php@resend.cc>
2014-02-20 05:28:39 +0200

    updated changelog

--- src/Runner.php
+++ src/Runner.php
@@ -11,7 +11,7 @@ use Symfony\Component\Console\Output\ConsoleOutput;
 
 class Runner {
 
-    const VERSION = '0.3.2';
+    const VERSION = '0.3.3';
     const ROBOCLASS = 'RoboFile';
     const ROBOFILE = 'RoboFile.php';
 

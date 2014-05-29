6b14bad5d27921699784cf051a410f4a6f2b6145
Peter Gasser <github@boedah.de>
2014-05-30 00:57:14 +0200

    add CommandStackTest (for issue #35)
    test as own commit to see the old behaviour

---
+++ tests/unit/CommandStackTest.php
@@ -0,0 +1,27 @@
+<?php
+use Codeception\Util\Stub;
+
+class CommandStackTest extends \Codeception\TestCase\Test
+{
+    public function testExecStackExecutableIsTrimmedFromCommand()
+    {
+        $commandStack = Stub::make('Robo\Task\Shared\CommandStack', array(
+            'executable' => 'some-executable'
+        ));
+        verify($commandStack
+                ->exec('some-executable status')
+                ->getCommand()
+        )->equals('some-executable status');
+    }
+
+    public function testExecStackCommandIsNotTrimmedIfHavingSameCharsAsExecutable()
+    {
+        $commandStack = Stub::make('Robo\Task\Shared\CommandStack', array(
+            'executable' => 'some-executable'
+        ));
+        verify($commandStack
+                ->exec('status')
+                ->getCommand()
+        )->equals('some-executable status');
+    }
+}

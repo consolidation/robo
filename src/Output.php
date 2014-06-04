0a17eb872ffc265ff56e905181aa0aff50b74359
Davert <davert.php@resend.cc>
2014-06-05 00:56:39 +0300

    merged with Output tests

--- src/Output.php
+++ src/Output.php
@@ -5,6 +5,9 @@ use Symfony\Component\Console\Helper\DialogHelper;
 use Symfony\Component\Console\Output\ConsoleOutput;
 
 trait Output {
+    private static $output;
+    private static $dialog;
+
     protected function say($text)
     {
         $this->writeln("➜  $text");
@@ -42,11 +45,28 @@ trait Output {
      */
     protected function getOutput()
     {
-        static $output;
-        if (!$output) {
-            $output = new ConsoleOutput();
+        if (!static::$output) {
+            static::$output = new ConsoleOutput();
         }
-        return $output;
+        return static::$output;
+    }
+
+    public static function setOutput(ConsoleOutput $output)
+    {
+        static::$output = $output;
+    }
+
+    protected function getDialogHelper()
+    {
+        if (!static::$dialog) {
+            static::$dialog = new DialogHelper();
+        }
+        return static::$dialog;
+    }
+
+    public static function setDialogHelper(DialogHelper $helper)
+    {
+        static::$dialog = $helper;
     }
 
     private function writeln($text)
---
+++ tests/unit/OutputTest.php
@@ -0,0 +1,59 @@
+<?php
+namespace Robo\Tests;
+
+use PHPUnit_Framework_TestCase;
+use Mockery as m;
+
+class OutputTest extends PHPUnit_Framework_TestCase {
+
+    private $output;
+    private $console;
+
+    const SAY_PREFIX = '';
+
+    public function setUp()
+    {
+        $this->output = new OutputStub;
+        $this->console = m::mock('Symfony\Component\Console\Output\ConsoleOutput');
+        OutputStub::setOutput($this->console);
+    }
+
+    public function testSay()
+    {
+        $this->console->shouldReceive('writeln')
+            ->with('➜  Hello, world!')
+            ->once();
+
+        $this->output->say('Hello, world!');
+    }
+
+    public function testAsk()
+    {
+        // Mock DialogHelper
+        $dialog = m::mock('Symfony\Component\Console\Helper\DialogHelper');
+        $dialog->shouldReceive('ask')
+            ->with($this->console, '<question>?  What is your name?</question> ');
+
+        OutputStub::setDialogHelper($dialog);
+
+        $result = $this->output->ask('What is your name?');
+    }
+}
+
+class OutputStub
+{
+    use Output {
+        Output::say as _say;
+        Output::ask as _ask;
+    }
+
+    public function say($text)
+    {
+        $this->_say($text);
+    }
+
+    public function ask($question)
+    {
+        $this->_ask($question);
+    }
+}

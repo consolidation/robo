3e95822ffe5ecd3f664a699c35b03ca2ccf24c55
Peter Gasser <github@boedah.de>
2014-12-04 01:22:16 +0100

    shell escape rsync exclude pattern
    fixes #77

--- tests/unit/RsyncTest.php
+++ tests/unit/RsyncTest.php
@@ -1,12 +1,12 @@
 <?php
 
-
 class RsyncTest extends \Codeception\TestCase\Test
 {
     use \Robo\Task\Rsync;
-   /**
-    * @var \CodeGuy
-    */
+
+    /**
+     * @var \CodeGuy
+     */
     protected $guy;
 
     // tests
@@ -14,20 +14,24 @@ class RsyncTest extends \Codeception\TestCase\Test
     {
         verify(
             $this->taskRsync()
-               ->fromPath('src/')
-               ->toHost('localhost')
-               ->toUser('dev')
-               ->toPath('/var/www/html/app/')
-               ->recursive()
-               ->excludeVcs()
-               ->checksum()
-               ->wholeFile()
-               ->verbose()
-               ->progress()
-               ->humanReadable()
-               ->stats()
-               ->getCommand()
-        )->equals('rsync --recursive --exclude .git/ --exclude .svn/ --exclude .hg/ --checksum --whole-file --verbose --progress --human-readable --stats src/ dev@localhost:/var/www/html/app/');
+                ->fromPath('src/')
+                ->toHost('localhost')
+                ->toUser('dev')
+                ->toPath('/var/www/html/app/')
+                ->recursive()
+                ->excludeVcs()
+                ->checksum()
+                ->wholeFile()
+                ->verbose()
+                ->progress()
+                ->humanReadable()
+                ->stats()
+                ->getCommand()
+        )->equals(sprintf('rsync --recursive --exclude %s --exclude %s --exclude %s --checksum --whole-file --verbose --progress --human-readable --stats src/ dev@localhost:/var/www/html/app/',
+            escapeshellarg('.git/'),
+            escapeshellarg('.svn/'),
+            escapeshellarg('.hg/')
+        ));
     }
 
 }

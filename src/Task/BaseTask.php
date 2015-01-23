16ebf217e090faa9694916cbd6b1ffc062a21586
Davert <davert.php@resend.cc>
2015-01-24 01:32:51 +0200

    dont print Stack suffix to console

--- src/Task/BaseTask.php
+++ src/Task/BaseTask.php
@@ -3,7 +3,6 @@ namespace Robo\Task;
 
 use Robo\Common\Configuration;
 use Robo\Common\TaskIO;
-use Robo\Common\Timer;
 use Robo\Contract\TaskInterface;
 
 abstract class BaseTask implements TaskInterface

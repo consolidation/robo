eef2a81eaffbe2c8856fbbdf954664eb28055994
Alessandro Astarita <aleast@caprionline.it>
2015-02-13 13:15:11 +0100

    new method replace() e regexReplace()

--- src/Task/File/Write.php
+++ src/Task/File/Write.php
@@ -31,33 +31,97 @@ class Write extends BaseTask
         $this->filename = $filename;
     }
 
+    /**
+     * add a line
+     *
+     * @param string $line
+     *
+     * @return Write The current instance
+     */
     public function line($line)
     {
         $this->body .= $line . "\n";
         return $this;
     }
 
-    public function lines($lines)
+    /**
+     * add more lines
+     *
+     * @param array $lines
+     *
+     * @return Write The current instance
+     */
+    public function lines(array $lines)
     {
         $this->body .= implode("\n", $lines) . "\n";
         return $this;
     }
 
+    /**
+     * add a text
+     *
+     * @param string $text
+     *
+     * @return Write The current instance
+     */
     public function text($text)
     {
         $this->body .= $text;
         return $this;
     }
 
+    /**
+     * add a text from a file
+     *
+     * @param string $filename
+     *
+     * @return Write The current instance
+     */
     public function textFromFile($filename)
     {
         $this->text(file_get_contents($filename));
         return $this;
     }
 
+    /**
+     * substitute a placeholder with value, placeholder must be enclosed by {}
+     *
+     * @param string $name
+     * @param string $val
+     *
+     * @return Write The current instance
+     */
     public function place($name, $val)
     {
-        $this->body = str_replace('{{' . $name . '}}', $val, $this->body);
+        $this->replace("{" . $name . "}", $val);
+        return $this;
+    }
+
+    /**
+     * replace any string with value
+     *
+     * @param string $string
+     * @param string $replacement
+     *
+     * @return Write The current instance
+     */
+    public function replace($string, $replacement)
+    {
+        $this->body = str_replace($string, $replacement, $this->body);
+        return $this;
+    }
+
+    /**
+     * replace any string with value using regular expression
+     *
+     * @param string $pattern
+     * @param string $replacement
+     *
+     * @return Write The current instance
+     */
+    public function regexReplace($pattern, $replacement)
+    {
+        $this->body = preg_replace($pattern, $replacement, $this->body);
         return $this;
     }
 

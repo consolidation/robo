a3286c98ac492f68d44835872234f9353c98bfe8
Davert <davert.php@resend.cc>
2015-02-24 04:00:54 +0200

    updated file/write docs

--- src/Task/File/Write.php
+++ src/Task/File/Write.php
@@ -84,7 +84,7 @@ class Write extends BaseTask
     }
 
     /**
-     * substitute a placeholder with value, placeholder must be enclosed by {{}}
+     * substitute a placeholder with value, placeholder must be enclosed by `{}`
      *
      * @param string $name
      * @param string $val

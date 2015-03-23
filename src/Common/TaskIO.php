7eef70ffd1d5d1063d216931947667eb804a0fa6
Florian Krämer <florian.kraemer@cakedc.com>
2015-03-23 14:06:25 +0100

    Displays the file size for the minified assets.
    It also fixes a division by zero error when an empty file was minified.

--- src/Common/TaskIO.php
+++ src/Common/TaskIO.php
@@ -23,6 +23,16 @@ trait TaskIO
         $this->writeln(" <fg=white;bg=red;options=bold>[$name]</fg=white;bg=red;options=bold> $text");
     }
 
+    protected function formatBytes($size, $precision = 2)
+    {
+        if ($size === 0) {
+            return 0;
+        }
+        $base = log($size, 1024);
+        $suffixes = array('', 'k', 'M', 'G', 'T');
+        return round(pow(1024, $base - floor($base)), $precision) . $suffixes[floor($base)];
+    }
+
     protected function getPrintedTaskName($task = null)
     {
         if (!$task) {

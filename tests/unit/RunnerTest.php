72f1c74d9ee9f319dd4b9bc9d333df6c78c0e9ca
aaukt <aaukt+github@posteo.de>
2015-03-23 21:25:46 +0100

    use custom error handler to correctly handle errors that were suppressed with the @-operator or by disabling error reporting

--- tests/unit/RunnerTest.php
+++ tests/unit/RunnerTest.php
@@ -94,6 +94,33 @@ class RunnerTest extends \Codeception\TestCase\Test
         $this->assertNotNull($this->app->find('generate:user-avatar'));
     }
 
+    public function testHandleError()
+    {
+        $tmpLevel = error_reporting();
+
+        $this->assertFalse($this->runner->handleError());
+        error_reporting(0);
+        $this->assertTrue($this->runner->handleError());
+
+        error_reporting($tmpLevel);
+    }
+
+    public function testErrorIsHandled()
+    {
+        $tmpLevel = error_reporting();
+
+        error_reporting(E_USER_ERROR);
+        set_error_handler(array($this->runner, 'handleError'));
+        @trigger_error('test error', E_USER_ERROR);
+        $this->assertEmpty(error_get_last());
+
+        error_reporting(0);
+        trigger_error('test error', E_USER_ERROR);
+        $this->assertEmpty(error_get_last());
+
+        error_reporting($tmpLevel);
+    }
+
     protected function createCommand($name)
     {
         return $this->runner->createCommand(new \Robo\TaskInfo(self::ROBOFILE, $name));

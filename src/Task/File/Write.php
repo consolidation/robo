dff706ff7dcd688f19785093eaa44da1df3e8257
Greg Anderson <greg.1.anderson@greenknowe.org>
2015-12-31 15:40:22 -0800

    Defer execution of operations in taskWriteToFile until the run() method, in case a file referenced in textFromFile() might not be created yet.

--- src/Task/File/Write.php
+++ src/Task/File/Write.php
@@ -23,7 +23,6 @@ class Write extends BaseTask
     use \Robo\Common\DynamicParams;
 
     protected $filename;
-    protected $body = "";
     protected $append = false;
 
     public function __construct($filename)
@@ -40,7 +39,7 @@ class Write extends BaseTask
      */
     public function line($line)
     {
-        $this->body .= $line . "\n";
+        $this->text($line . "\n");
         return $this;
     }
 
@@ -53,7 +52,7 @@ class Write extends BaseTask
      */
     public function lines(array $lines)
     {
-        $this->body .= implode("\n", $lines) . "\n";
+        $this->text(implode("\n", $lines) . "\n");
         return $this;
     }
 
@@ -66,12 +65,18 @@ class Write extends BaseTask
      */
     public function text($text)
     {
-        $this->body .= $text;
+        $this->stack[] = array_merge(['_' . __FUNCTION__], func_get_args());
         return $this;
     }
 
     /**
      * add a text from a file
+     *
+     * Note that the file is read in the run() method of this task.
+     * To load text from the current state of a file (e.g. one that may
+     * be deleted or altered by other tasks prior the execution of this one),
+     * use:
+     *       $task->text(file_get_contents($filename));
      *
      * @param string $filename
      *
@@ -79,7 +84,7 @@ class Write extends BaseTask
      */
     public function textFromFile($filename)
     {
-        $this->text(file_get_contents($filename));
+        $this->stack[] = array_merge(['_' . __FUNCTION__], func_get_args());
         return $this;
     }
 
@@ -107,7 +112,7 @@ class Write extends BaseTask
      */
     public function replace($string, $replacement)
     {
-        $this->body = str_replace($string, $replacement, $this->body);
+        $this->stack[] = array_merge(['_' . __FUNCTION__], func_get_args());
         return $this;
     }
 
@@ -121,20 +126,49 @@ class Write extends BaseTask
      */
     public function regexReplace($pattern, $replacement)
     {
-        $this->body = preg_replace($pattern, $replacement, $this->body);
+        $this->stack[] = array_merge(['_' . __FUNCTION__], func_get_args());
         return $this;
+    }
+
+    public function _textFromFile($body, $filename) {
+        return $body . file_get_contents($filename);
+    }
+
+    public function _replace($body, $string, $replacement) {
+        return str_replace($string, $replacement, $body);
+    }
+
+    public function _regexReplace($body, $pattern, $replacement) {
+        return preg_replace($pattern, $replacement, $body);
+    }
+
+    public function _text($body, $text) {
+        return $body . $text;
+    }
+
+    public function buildContents() {
+        $body = "";
+        if ($this->append) {
+            $body = file_get_contents($this->filename);
+        }
+        foreach ($this->stack as $action) {
+            $command = array_shift($action);
+            if (method_exists($this, $command)) {
+                array_unshift($action, $body);
+                $body = call_user_func_array([$this, $command], $action);
+            }
+        }
+        return $body;
     }
 
     public function run()
     {
         $this->printTaskInfo("Writing to <info>{$this->filename}</info>.");
-        if ($this->append) {
-            $this->body = file_get_contents($this->filename) . $this->body;
-        }
+        $body = $this->buildContents();
         if (!file_exists(dirname($this->filename))) {
             mkdir(dirname($this->filename), 0777, true);
         }
-        $res = file_put_contents($this->filename, $this->body);
+        $res = file_put_contents($this->filename, $body);
         if ($res === false) {
             return Result::error($this, "File {$this->filename} couldn't be created");
         }

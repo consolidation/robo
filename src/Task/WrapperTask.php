5818a815d9e9a3d6c9919dfb81336b72dd261424
Greg Anderson <greg.1.anderson@greenknowe.org>
2016-01-10 06:44:11 -0800

    If a wrapped class contains simple getters and setters, they should be called immediately, and not deferred until run().

--- src/Task/WrapperTask.php
+++ src/Task/WrapperTask.php
@@ -122,12 +122,33 @@ abstract class WrapperTask extends BaseTask {
     {
         foreach ($this->getDelegateCommandList($function) as $command) {
             if (method_exists($command[0], $command[1])) {
+                // If the function is a getter, call it now and
+                // return the result.
+                if ($this->isGetter($command[1])) {
+                    return call_user_func_array($command, $args);
+                }
+                // If the function is a setter, call it now, and
+                // return $this.
+                if ($this->isSetter($command[1])) {
+                    call_user_func_array($command, $args);
+                    return $this;
+                }
+                // Otherwise, we'll defer calling this function
+                // until run(), and return $this.
                 $this->addToCommandStack($command, $args);
                 return $this;
             }
         }
 
         die("Method $function does not exist.\n");
+    }
+
+    protected function isGetter($function) {
+        return substr($function, 0, 3) == "get";
+    }
+
+    protected function isSetter($function) {
+        return substr($function, 0, 3) == "set";
     }
 
     /**

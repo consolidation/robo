ff57fa9a46a146047736671ec75d5819ee5b4d9e
Greg Anderson <greg.1.anderson@greenknowe.org>
2016-02-16 11:43:12 -0800

    Allow tasks to pass contexts into TaskIO methods. May still provide a $task, which is converted into ['task' => $task].

--- src/Common/TaskIO.php
+++ src/Common/TaskIO.php
@@ -8,22 +8,22 @@ trait TaskIO
 {
     use IO;
 
-    protected function printTaskInfo($text, $task = null)
+    protected function printTaskInfo($text, $context = null)
     {
         // The 'note' style is used for both 'notice' and 'info' log levels;
         // However, 'notice' is printed at VERBOSITY_NORMAL, whereas 'info'
         // is only printed at VERBOSITY_VERBOSE.
-        Config::logger()->notice($text, $this->getTaskContext($task));
+        Config::logger()->notice($text, $this->getTaskContext($context));
     }
 
-    protected function printTaskSuccess($text, $task = null)
+    protected function printTaskSuccess($text, $context = null)
     {
-        Config::logger()->success($text, $this->getTaskContext($task));
+        Config::logger()->success($text, $this->getTaskContext($context));
     }
 
-    protected function printTaskError($text, $task = null)
+    protected function printTaskError($text, $context = null)
     {
-        Config::logger()->error($text, $this->getTaskContext($task));
+        Config::logger()->error($text, $this->getTaskContext($context));
     }
 
     protected function formatBytes($size, $precision = 2)
@@ -44,11 +44,18 @@ trait TaskIO
         return TaskInfo::formatTaskName($task);
     }
 
-    protected function getTaskContext($task = null)
+    protected function getTaskContext($context = null)
     {
-        if (!$task) {
-            $task = $this;
+        if (!$context) {
+            $context = [];
         }
-        return TaskInfo::getTaskContext($task);
+        if (!is_array($context)) {
+            $context = ['task' => $context];
+        }
+        if (!array_key_exists('task', $context)) {
+            $context['task'] = $this;
+        }
+
+        return $context + TaskInfo::getTaskContext($context['task']);
     }
 }

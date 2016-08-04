a5a57fde97e4f2602d3c5297e81a0c006b92995f
Davert <davert.php@resend.cc>
2016-08-05 01:15:45 +0300

    updated robofile to publish tags

--- RoboFile.php
+++ RoboFile.php
@@ -84,7 +84,7 @@ class RoboFile extends \Robo\Tasks
             $stable = false;
             $this->say('non-stable release');
         }
-        
+
         $releaseDescription = $this->ask("Description of Release\n");
 
         $this->docs();
@@ -98,11 +98,9 @@ class RoboFile extends \Robo\Tasks
         if ($stable) $this->pharPublish();
         $this->publish();
 
-        $this->taskGitHubRelease(\Robo\Runner::VERSION)
-            ->user($this->ask('User releasing'))
-            ->password($this->askHidden('Password'))
-            ->uri('Codegyre/Robo')
-            ->description($releaseDescription)
+        $this->taskGitStack()
+            ->tag(\Robo\Runner::VERSION)
+            ->push('origin master --tags')
             ->run();
 
         if ($stable) $this->versionBump();

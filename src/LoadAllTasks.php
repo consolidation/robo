04fee1794e55654b0ab2cda4fcd454f2cbdf0a44
Greg Anderson <greg.1.anderson@greenknowe.org>
2016-08-09 15:23:17 -0700

    Create a static Robo::standardServices() method to provide Robo's standard service list. This makes it easier for external programs that use Robo as a library to configure their DI container. (#403)

--- src/LoadAllTasks.php
+++ src/LoadAllTasks.php
@@ -5,33 +5,6 @@ trait LoadAllTasks
 {
     use TaskAccessor;
 
-    /**
-     * Return all of the service providers needed by the RoboFile.
-     * By default, we return all of the built-in Robo task providers.
-     */
-    public function getServiceProviders()
-    {
-        return
-        [
-            \Robo\Collection\loadTasks::getCollectionServices(),
-            \Robo\Task\ApiGen\loadTasks::getApiGenServices(),
-            \Robo\Task\Archive\loadTasks::getArchiveServices(),
-            \Robo\Task\Assets\loadTasks::getAssetsServices(),
-            \Robo\Task\Base\loadTasks::getBaseServices(),
-            \Robo\Task\Npm\loadTasks::getNpmServices(),
-            \Robo\Task\Bower\loadTasks::getBowerServices(),
-            \Robo\Task\Gulp\loadTasks::getGulpServices(),
-            \Robo\Task\Composer\loadTasks::getComposerServices(),
-            \Robo\Task\Development\loadTasks::getDevelopmentServices(),
-            \Robo\Task\Docker\loadTasks::getDockerServices(),
-            \Robo\Task\File\loadTasks::getFileServices(),
-            \Robo\Task\Filesystem\loadTasks::getFilesystemServices(),
-            \Robo\Task\Remote\loadTasks::getRemoteServices(),
-            \Robo\Task\Testing\loadTasks::getTestingServices(),
-            \Robo\Task\Vcs\loadTasks::getVcsServices(),
-        ];
-    }
-
     use Collection\loadTasks;
 
     // standard tasks

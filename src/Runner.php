7ec4e7cfd88b245bca763ba1b1d71aa2f9484626
Greg Anderson <greg.1.anderson@greenknowe.org>
2016-09-09 17:33:23 -0700

    Simplify container initialization.

--- src/Runner.php
+++ src/Runner.php
@@ -112,19 +112,15 @@ class Runner implements ContainerAwareInterface
         $this->setInput($input);
         $this->setOutput($output);
 
-        // If our client gave us a container, then also set it inside
-        // the static Robo class.
-        if ($this->getContainer()) {
-            Robo::setContainer($this->getContainer());
-        }
         // If we were not provided a container, then create one
-        if (!Robo::hasContainer()) {
-            Robo::createDefaultContainer($input, $output, $app);
-            $this->setContainer(Robo::getContainer());
+        if (!$this->getContainer()) {
+            $container = Robo::createDefaultContainer($input, $output, $app);
+            $this->setContainer($container);
             // Automatically register a shutdown function and
             // an error handler when we provide the container.
             $this->installRoboHandlers();
         }
+
         if (!$app) {
             $app = Robo::application();
         }

e7a63c79756a3aec84a482bb4d8f193e51305286
Fabien Villepinte <fabien.villepinte@gmail.com>
2017-01-19 16:45:13 +0100

    Fix: use baseDir when provided as a parameter in the TmpFile constructor

--- src/Task/File/TmpFile.php
+++ src/Task/File/TmpFile.php
@@ -35,15 +35,15 @@ class TmpFile extends Write implements CompletionInterface
      */
     public function __construct($filename = 'tmp', $extension = '', $baseDir = '', $includeRandomPart = true)
     {
-        if (empty($base)) {
-            $base = sys_get_temp_dir();
+        if (empty($baseDir)) {
+            $baseDir = sys_get_temp_dir();
         }
         if ($includeRandomPart) {
             $random = static::randomString();
             $filename = "{$filename}_{$random}";
         }
         $filename .= $extension;
-        parent::__construct("{$base}/{$filename}");
+        parent::__construct("{$baseDir}/{$filename}");
     }
 
     /**

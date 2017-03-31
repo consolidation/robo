de6225256b2ed88822af1cd8cc3a01bf933add8c
Greg Anderson <greg.1.anderson@greenknowe.org>
2017-03-31 11:23:36 -0700

    Robo release 1.0.6

--- src/Robo.php
+++ src/Robo.php
@@ -18,7 +18,7 @@ use Symfony\Component\Process\Process;
 class Robo
 {
     const APPLICATION_NAME = 'Robo';
-    const VERSION = '1.0.6-dev';
+    const VERSION = '1.0.6';
 
     /**
      * The currently active container object, or NULL if not initialized yet.

b465f217e324ce5a2ab61ecd5a31a20525b1d172
Matthew Grasmick <matt.grasmick@gmail.com>
2017-04-14 15:58:14 -0400

    Updating tests to call interactive() explicitly.

--- tests/cli/ExecCest.php
+++ tests/cli/ExecCest.php
@@ -6,7 +6,7 @@ class ExecCest
     public function toExecLsCommand(CliGuy $I)
     {
         $command = strncasecmp(PHP_OS, 'WIN', 3) == 0 ? 'dir' : 'ls';
-        $res = $I->taskExec($command)->run();
+        $res = $I->taskExec($command)->interactive(false)->run();
         verify($res->getMessage())->contains('src');
         verify($res->getMessage())->contains('codeception.yml');
     }

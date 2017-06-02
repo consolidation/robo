b7fe5952085dd5e73a3aa64a7394b8c95a0e6110
Alex Skrypnyk <alex.designworks@gmail.com>
2017-06-02 13:25:52 +1000

    [#582] Added test for muti-level relative file exclusion in CopyDir.

--- tests/cli/CopyDirRecursiveExcludeCept.php
+++ tests/cli/CopyDirRecursiveExcludeCept.php
@@ -7,6 +7,8 @@ $I->seeDirFound('some/deeply/nested');
 $I->seeDirFound('some/deeply/nested2');
 $I->seeDirFound('some/deeply/nested3');
 $I->seeDirFound('some/deeply/nested3/nested31');
+$I->seeDirFound('some/deeply/nested4');
+$I->seeDirFound('some/deeply/nested4/nested41');
 $I->seeFileFound('structu.re', 'some/deeply/nested');
 $I->seeFileFound('structu1.re', 'some/deeply/nested');
 $I->seeFileFound('structu2.re', 'some/deeply/nested');
@@ -15,6 +17,8 @@ $I->seeFileFound('structu21.re', 'some/deeply/nested2');
 $I->seeFileFound('structu31.re', 'some/deeply/nested3');
 $I->seeFileFound('structu32.re', 'some/deeply/nested3');
 $I->seeFileFound('structu311.re', 'some/deeply/nested3/nested31');
+$I->seeFileFound('structu411.re', 'some/deeply/nested4/nested41');
+$I->seeFileFound('structu412.re', 'some/deeply/nested4/nested41');
 $I->taskCopyDir(['some/deeply' => 'some_destination/deeply'])
     ->exclude([
         // Basename exclusion.
@@ -27,6 +31,8 @@ $I->taskCopyDir(['some/deeply' => 'some_destination/deeply'])
         'some/deeply/nested3/nested31',
         // Subpath within source exclusion.
         'nested3/structu31.re',
+        // File in deeper subpath within source exclusion.
+        'nested4/nested41/structu411.re',
     ])
     ->run();
 $I->seeDirFound('some_destination/deeply/nested');
@@ -39,3 +45,5 @@ $I->seeDirFound('some_destination/deeply/nested3');
 $I->cantSeeFileFound('structu31.re', 'some_destination/deeply/nested3');
 $I->canSeeFileFound('structu32.re', 'some_destination/deeply/nested3');
 $I->cantSeeFileFound('nested31', 'some_destination/deeply/nested3');
+$I->cantSeeFileFound('structu411.re', 'some_destination/deeply/nested4/nested41');
+$I->canSeeFileFound('structu412.re', 'some_destination/deeply/nested4/nested41');

bfd2db6efabb64b03950b10f3381fbaed9c990e7
Greg Anderson <greg.1.anderson@greenknowe.org>
2017-06-02 16:44:40 -0700

    Enhance SemVer task so that it can be used with files or strings. (#589)

--- src/Collection/Collection.php
+++ src/Collection/Collection.php
@@ -567,6 +567,8 @@ class Collection extends BaseTask implements CollectionInterface, CommandInterfa
                 // the incremental results, if they wish.
                 $key = Result::isUnnamed($taskName) ? $name : $taskName;
                 $result->accumulate($key, $taskResult);
+                // The result message will be the message of the last task executed.
+                $result->setMessage($taskResult->getMessage());
             }
         } catch (TaskExitException $exitException) {
             $this->fail();

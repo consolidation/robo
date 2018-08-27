a2ed34fe7000f0232bd8d0c56c0c84f82ec7ac07
James Sansbury <james@lullabot.com>
2018-08-27 12:08:35 -0400

    Add tests to ensure that environment variables are preserved in Exec #768

--- tests/cli/ExecCest.php
+++ tests/cli/ExecCest.php
@@ -10,4 +10,31 @@ class ExecCest
         verify($res->getMessage())->contains('src');
         verify($res->getMessage())->contains('codeception.yml');
     }
+
+    public function testMultipleEnvVars(CliGuy $I)
+    {
+        $task = $I->taskExec('env')->interactive(false);
+        $task->env('FOO', 'BAR');
+        $task->env('BAR', 'BAZ');
+        $result = $task->run();
+        // Verify that the text contains our environment variable.
+        verify($result->getMessage())->contains('FOO=BAR');
+        verify($result->getMessage())->contains('BAR=BAZ');
+    }
+
+    public function testInheritEnv(CliGuy $I)
+    {
+        // With no environment variables set, test that we have a known variable
+        // such as PATH.
+        $task = $I->taskExec('env | grep -E "^PATH="')->interactive(false);
+        $result = $task->run();
+        verify($result->getExitCode())->equals(\Robo\Result::EXITCODE_OK);
+
+        // Now run the same command, but this time set an environment variable
+        // on the task using ->env().
+        $task = $I->taskExec('env | grep -E "^PATH="')->interactive(false);
+        $task->env('FOO', 'BAR');
+        $result = $task->run();
+        verify($result->getExitCode())->equals(\Robo\Result::EXITCODE_OK);
+    }
 }

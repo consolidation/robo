588621124325003af793c990a32d60a76849f941
Greg Anderson <greg.1.anderson@greenknowe.org>
2019-02-15 18:08:08 -0800

    Extend ConfigAwareTrait from consolidation config (#838)

--- src/Contract/ConfigAwareInterface.php
+++ src/Contract/ConfigAwareInterface.php
@@ -2,23 +2,6 @@
 
 namespace Robo\Contract;
 
-use Consolidation\Config\ConfigInterface;
-
-interface ConfigAwareInterface
+interface ConfigAwareInterface extends \Consolidation\Config\ConfigAwareInterface
 {
-    /**
-     * Set the config reference
-     *
-     * @param ConfigInterface $config
-     *
-     * @return $this
-     */
-    public function setConfig(ConfigInterface $config);
-
-    /**
-     * Get the config reference
-     *
-     * @return ConfigInterface
-     */
-    public function getConfig();
 }

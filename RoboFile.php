3482a93534028bec7fbdb4365fef096d80c2656f
Greg Anderson <greg.1.anderson@greenknowe.org>
2019-03-19 08:45:25 -0700

    Correct RELEASE script

--- RoboFile.php
+++ RoboFile.php
@@ -94,17 +94,14 @@ class RoboFile extends \Robo\Tasks
             ->push()
             ->run();
 
-        if ($stable) {
-            $this->pharPublish();
-        }
         $this->publish();
-
         $this->taskGitStack()
             ->tag($version)
             ->push('origin master --tags')
             ->run();
 
         if ($stable) {
+            $this->pharPublish();
             $version = $this->incrementVersion($version) . '-dev';
             $this->writeVersion($version);
 

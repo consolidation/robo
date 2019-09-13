54bb7eae43ef19defe0d1ff3e65e32e9fab916d5
Greg Anderson <greg.1.anderson@greenknowe.org>
2019-09-13 13:41:15 -0700

    Robo 2.0.0-alpha2

--- src/Robo.php
+++ src/Robo.php
@@ -20,7 +20,7 @@ use Symfony\Component\Process\Process;
 class Robo
 {
     const APPLICATION_NAME = 'Robo';
-    const VERSION = '2.0.0-dev';
+    const VERSION = '2.0.0-alpha2';
 
     /**
      * The currently active container object, or NULL if not initialized yet.

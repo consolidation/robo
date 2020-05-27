182af575decf36bab4444399bd6fbfa7180e9c48
Greg Anderson <greg.1.anderson@greenknowe.org>
2020-05-27 15:14:45 -0700

    Prepare for 2.1.1-dev

--- src/Robo.php
+++ src/Robo.php
@@ -21,7 +21,7 @@ use Symfony\Component\Process\Process;
 class Robo
 {
     const APPLICATION_NAME = 'Robo';
-    const VERSION = '2.1.0';
+    const VERSION = '2.1.1-dev';
 
     /**
      * The currently active container object, or NULL if not initialized yet.

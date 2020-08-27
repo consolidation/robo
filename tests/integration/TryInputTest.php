be53befb1efb87fb30bc48f66af063152f0437b1
Les Peabody <lpeabody@gmail.com>
2020-08-27 18:19:13 -0400

    Nitpicks.

---
+++ tests/integration/TryInputTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace Robo;
+
+use PHPUnit\Framework\TestCase;
+use Robo\Traits\CommandTesterTrait;
+use RoboExample\Robo\Plugin\Commands\ExampleCommands;
+
+class TryInputTestTest extends TestCase
+{
+    use CommandTesterTrait;
+
+    public function setUp()
+    {
+        $this->setupCommandTester(ExampleCommands::class);
+    }
+
+    public function testInputApis()
+    {
+        list($tryInputOutput, $statusCode) = $this->executeCommand('try:input', ["I'm great!", "yes", "PHP", "1234"]);
+        $this->assertEquals(0, $statusCode);
+        $this->assertContains("I'm great!", $tryInputOutput);
+        $this->assertContains("PHP", $tryInputOutput);
+        $this->assertContains("1234", $tryInputOutput);
+    }
+}

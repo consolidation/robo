17474b5d75ff82009bf1f0c4247befa4be646c58
Dan Untenzu <untenzu@webit.de>
2021-11-05 17:07:49 +0100

    [FEATURE] Image Minify: Add test for minification
    Test image minification.
    
    Test starts red due to wrong executable paths
    in the existing task constructor.
    
    This will be fixed in the following commits.

--- tests/integration/AssetsTest.php
+++ tests/integration/AssetsTest.php
@@ -25,29 +25,49 @@ class AssetsTest extends TestCase
     public function testCssMinification()
     {
         if (version_compare(PHP_VERSION, '7.2.0') >= 0) {
-          $this->markTestSkipped('natxet/cssmin uses deprecated "each()" function (PHP 7.2+)');
+            $this->markTestSkipped('natxet/cssmin uses deprecated "each()" function (PHP 7.2+)');
         }
 
         $this->fixtures->createAndCdToSandbox();
 
-				$sampleCss = $this->fixtures->dataFile('sample.css');
-				$outputCss = 'minifiedSample.css';
+        $sampleCss = $this->fixtures->dataFile('sample.css');
+        $outputCss = 'minifiedSample.css';
 
-				$initialFileSize = filesize($sampleCss);
+        $initialFileSize = filesize($sampleCss);
 
-				$result = $this->taskMinify($sampleCss)
-				  ->to('minifiedSample.css')
-				  ->run();
+        $result = $this->taskMinify($sampleCss)
+            ->to('minifiedSample.css')
+            ->run();
         $this->assertTrue($result->wasSuccessful(), $result->getMessage());
 
-				$this->assertFileExists($outputCss);
-				$minifiedFileSize = filesize($outputCss);
-				$outputCssContents = file_get_contents($outputCss);
+        $this->assertFileExists($outputCss);
+        $minifiedFileSize = filesize($outputCss);
+        $outputCssContents = file_get_contents($outputCss);
 
-				$this->assertLessThan($initialFileSize, $minifiedFileSize, 'Minified file is smaller than the source file');
-				$this->assertGreaterThan(0, $minifiedFileSize, 'Minified file is not empty');
-				$this->assertStringContainsString('body', $outputCssContents, 'Minified file has some content from the source file');
-				$this->assertStringNotContainsString('Sample css file', $outputCssContents, 'Minified file does not contain comment from source file');
+        $this->assertLessThan($initialFileSize, $minifiedFileSize, 'Minified file is smaller than the source file');
+        $this->assertGreaterThan(0, $minifiedFileSize, 'Minified file is not empty');
+        $this->assertStringContainsString('body', $outputCssContents, 'Minified file has some content from the source file');
+        $this->assertStringNotContainsString('Sample css file', $outputCssContents, 'Minified file does not contain comment from source file');
     }
 
+    public function testImageMinification()
+    {
+        $this->fixtures->createAndCdToSandbox();
+
+        $sampleImage = $this->fixtures->dataFile('sample.png');
+        $outputImage = 'dist/sample.png';
+
+        $initialFileSize = filesize($sampleImage);
+
+        $result = $this->taskImageMinify($sampleImage)
+            ->to('dist/')
+            ->run();
+        $this->assertTrue($result->wasSuccessful(), $result->getMessage());
+
+        $this->assertFileExists($outputImage);
+        $minifiedFileSize = filesize($outputImage);
+
+        $this->assertLessThan($initialFileSize, $minifiedFileSize, 'Minified file is smaller than the source file');
+        $this->assertGreaterThan(0, $minifiedFileSize, 'Minified file is not empty');
+    }
 }

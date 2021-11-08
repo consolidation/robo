060ef07b2d5aa5bc6ae6608058597d607bad0027
Dan Untenzu <untenzu@webit.de>
2021-11-08 17:45:47 +0100

    Backport Fix image minifier task to 2.x (#1054)
    * [FEATURE] Image Minify: Add test for minification
    
    Test image minification.
    
    Test starts red due to wrong executable paths
    in the existing task constructor.
    
    This will be fixed in the following commits.
    
    * [BUGFIX] Image Minify: Avoid creation of null directory
    
    If the executableTargetDir was not set yet,
    because the calculation in the constructor was faulty,
    then return early and do not try to create a null directory.
    
    refs #1051
    
    * [FEATURE] Image Minify: Add task command to set bin dir
    
    Add task command to set a directory used to load
    the executables from and/or download them into.
    
    Refs #1051
    
    * [BUGFIX] Image Minify: Change source URL for binaries
    
    The source repository for the image minifier downloader
    changed its main branch name. This causes the downloader to
    download 404 pages instead of binaries.
    Change the branch name in the URL to download the expected
    binary files again.
    
    Refs #1050
    
    * [FEATURE] Image Minify: Skip test on windows
    
    Right now it is not possible to run the test on Windows
    due to several issues in the imagemin downloader.
    
    The task runs without any minification on windows.
    So the following checks are superfluous.
    
    Therefore it should be skipped for now on win.
    The linux tests still work as expected.
    
    Refs #1052 #1053

--- tests/integration/AssetsTest.php
+++ tests/integration/AssetsTest.php
@@ -25,29 +25,55 @@ class AssetsTest extends TestCase
     public function testCssMinification()
     {
         if (version_compare(PHP_VERSION, '7.2.0') >= 0) {
-          $this->markTestSkipped('natxet/cssmin uses deprecated "each()" function (PHP 7.2+)');
+            $this->markTestSkipped('natxet/cssmin uses deprecated "each()" function (PHP 7.2+)');
         }
 
         $this->fixtures->createAndCdToSandbox();
 
-				$sampleCss = $this->fixtures->dataFile('sample.css');
-				$outputCss = 'minifiedSample.css';
+        $sampleCss = $this->fixtures->dataFile('sample.css');
+        $outputCss = 'minifiedSample.css';
 
-				$initialFileSize = filesize($sampleCss);
+        $initialFileSize = filesize($sampleCss);
 
-				$result = $this->taskMinify($sampleCss)
-				  ->to('minifiedSample.css')
-				  ->run();
+        $result = $this->taskMinify($sampleCss)
+            ->to('minifiedSample.css')
+            ->run();
         $this->assertTrue($result->wasSuccessful(), $result->getMessage());
 
-				$this->assertFileExists($outputCss);
-				$minifiedFileSize = filesize($outputCss);
-				$outputCssContents = file_get_contents($outputCss);
+        $this->assertFileExists($outputCss);
+        $minifiedFileSize = filesize($outputCss);
+        $outputCssContents = file_get_contents($outputCss);
 
-				$this->assertLessThan($initialFileSize, $minifiedFileSize, 'Minified file is smaller than the source file');
-				$this->assertGreaterThan(0, $minifiedFileSize, 'Minified file is not empty');
-				$this->assertContains('body', $outputCssContents, 'Minified file has some content from the source file');
-				$this->assertNotContains('Sample css file', $outputCssContents, 'Minified file does not contain comment from source file');
+        $this->assertLessThan($initialFileSize, $minifiedFileSize, 'Minified file is smaller than the source file');
+        $this->assertGreaterThan(0, $minifiedFileSize, 'Minified file is not empty');
+        $this->assertContains('body', $outputCssContents, 'Minified file has some content from the source file');
+        $this->assertNotContains('Sample css file', $outputCssContents, 'Minified file does not contain comment from source file');
     }
 
+    public function testImageMinification()
+    {
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            $this->markTestSkipped('Binary download & execution not working on Windows (#1053).');
+        }
+
+        $this->fixtures->createAndCdToSandbox();
+        mkdir('dist');
+
+        $sampleImage = $this->fixtures->dataFile('sample.png');
+        $outputImage = 'dist/sample.png';
+
+        $initialFileSize = filesize($sampleImage);
+
+        $result = $this->taskImageMinify($sampleImage)
+            ->setExecutableDir(realpath('') . '/bin') // use sandbox for bin download
+            ->to(realpath('') . '/dist')
+            ->run();
+        $this->assertTrue($result->wasSuccessful(), $result->getMessage());
+
+        $this->assertFileExists($outputImage);
+        $minifiedFileSize = filesize($outputImage);
+
+        $this->assertLessThan($initialFileSize, $minifiedFileSize, 'Minified file is smaller than the source file');
+        $this->assertGreaterThan(0, $minifiedFileSize, 'Minified file is not empty');
+    }
 }

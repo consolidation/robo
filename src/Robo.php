91d64f9a7652b8c21761e9e24113e54b6947a7ee
Greg Anderson <greg.1.anderson@greenknowe.org>
2022-04-27 08:10:06 -0700

    Release 4.0.0-alpha2

--- src/Robo.php
+++ src/Robo.php
@@ -22,7 +22,7 @@ use Symfony\Component\Process\Process;
 class Robo
 {
     const APPLICATION_NAME = 'Robo';
-    private const VERSION = '4.0.0-alpha1';
+    private const VERSION = '4.0.0-alpha2';
 
     /**
      * The currently active container object, or NULL if not initialized yet.

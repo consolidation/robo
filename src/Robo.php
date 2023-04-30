013af6c52469541720393d88f2dc6d417419e75a
Greg Anderson <greg.1.anderson@greenknowe.org>
2023-04-30 14:49:48 -0700

    Prepare for 4.0.7-dev

--- src/Robo.php
+++ src/Robo.php
@@ -26,7 +26,7 @@ use Symfony\Component\Process\Process;
 class Robo
 {
     const APPLICATION_NAME = 'Robo';
-    private const VERSION = '4.0.6';
+    private const VERSION = '4.0.7-dev';
 
     /**
      * The currently active container object, or NULL if not initialized yet.

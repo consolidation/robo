efd827c22fe4f9e87fc58363d3399ccdae809ead
Chris Yates <beastbytes@users.noreply.github.com>
2025-02-18 23:01:22 +0000

    Correct REGEX constant
    Make metadata compliant with [Semantic Versioning 2.0.0](https://semver.org/)

--- src/Task/Development/SemVer.php
+++ src/Task/Development/SemVer.php
@@ -22,7 +22,7 @@ class SemVer implements TaskInterface
 {
     const SEMVER = "---\n:major: %d\n:minor: %d\n:patch: %d\n:special: '%s'\n:metadata: '%s'";
 
-    const REGEX = "/^\-\-\-\r?\n:major:\s(0|[1-9]\d*)\r?\n:minor:\s(0|[1-9]\d*)\r?\n:patch:\s(0|[1-9]\d*)\r?\n:special:\s'([a-zA-z0-9]*\.?(?:0|[1-9]\d*)?)'\r?\n:metadata:\s'((?:0|[1-9]\d*)?(?:\.[a-zA-z0-9\.]*)?)'/";
+    const REGEX = "/^\-\-\-\r?\n:major:\s(0|[1-9]\d*)\r?\n:minor:\s(0|[1-9]\d*)\r?\n:patch:\s(0|[1-9]\d*)\r?\n:special:\s'([a-zA-z0-9]*\.?(?:0|[1-9]\d*)?)'\r?\n:metadata:\s'([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*)'/";
 
     const REGEX_STRING = '/^(?<major>[0-9]+)\.(?<minor>[0-9]+)\.(?<patch>[0-9]+)(|-(?<special>[0-9a-zA-Z.]+))(|\+(?<metadata>[0-9a-zA-Z.]+))$/';
 

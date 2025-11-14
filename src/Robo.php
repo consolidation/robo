f391d50a8b4f58ee8022373d9abbc62c9a4664ac
Greg Anderson <greg@pantheon.io>
2025-11-14 15:30:22 -0800

    Prepare for 5.1.2-dev

--- src/Robo.php
+++ src/Robo.php
@@ -26,7 +26,7 @@ use Symfony\Component\Process\Process;
 class Robo
 {
     const APPLICATION_NAME = 'Robo';
-    private const VERSION = '5.1.1';
+    private const VERSION = '5.1.2-dev';
 
     /**
      * The currently active container object, or NULL if not initialized yet.
